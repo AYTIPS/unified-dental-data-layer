57659bdbaa788c2fbca50418db92702093f6bfaa
Ayomide <AYTIPS@example.com>
2026-03-10 12:54:42 +0100

    new added redis

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ class Settings (BaseSettings):
     refresh_token_expire_days :   int = Field(..., env = ("refresh_token_expire_days", "REFRESH_TOKEN_EXPIRE_DAYS")) # type: ignore  
     encryption_key : str = Field(..., env = ("encryption_key",  "ENCRYPTION_KEY"))  # type: ignore
     invite_ttl_hours: int = Field(..., env = ("invite_ttl_hours", "INVITE_TTL_HOURS")) # type: ignore
+    redis_url: str = Field(..., env=("redis_url", "REDIS_URL"))  # type: ignore
+
 
 
 
--- core/queue.py
+++ core/queue.py
@@ -1,17 +1,13 @@
 from redis import Redis
 from redis.asyncio import Redis as AsyncRedis
 from rq import Queue
-
-redis_client = Redis(
-    host="localhost",
-    port=6379,
-    db=0,
+from config import settings
+redis_client = Redis.from_url(
+    settings.redis_url,
     decode_responses=True,
 )
-async_redis = AsyncRedis(
-    host="localhost",
-    port=6379,
-    db=0,
+async_redis = AsyncRedis.from_url(
+    settings.redis_url,
     decode_responses=True,
 )
 
